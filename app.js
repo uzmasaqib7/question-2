f986ddab24f932b2508b047fc1e772515afe7019
unknown <Wardan Ahmed>
2024-05-19 02:23:15 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,2 @@
+var personName = "Uzma Saqib";
+console.log("hello ".concat(personName, " would you like to learn some python today?"));
---
+++ app.ts
@@ -0,0 +1,2 @@
+let personName = "Uzma Saqib"; 
+console.log(`hello ${personName} would you like to learn some python today?`);
